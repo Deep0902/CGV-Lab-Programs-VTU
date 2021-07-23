aa206cfcebafe24c94d2933cbd6d4f692bf22d82
Deep Rakesh <deeptank09@gmail.com>
2021-07-23 11:43:04 +0530

    Update Program 5

--- Program 5 Cohen-Sutherland Algorithm/main.cpp
+++ Program 5 Cohen-Sutherland Algorithm/main.cpp
@@ -7,7 +7,8 @@ const int LEFT = 1;
 const int RIGHT = 2;
 const int BOTTOM = 4;
 const int TOP = 8;
-int ComputeOutCode(double x, double y) {
+int ComputeOutCode(double x, double y)
+{
   int code = 0;
   if (y > ymax)
     code |= TOP;
@@ -19,40 +20,54 @@ int ComputeOutCode(double x, double y) {
     code |= LEFT;
   return code;
 }
-void CohenSutherland(double x0, double y0, double x1, double y1) {
+void CohenSutherland(double x0, double y0, double x1, double y1)
+{
   int outcode0, outcode1, outcodeOut;
   bool accept = false, done = false;
   outcode0 = ComputeOutCode(x0, y0);
   outcode1 = ComputeOutCode(x1, y1);
-  do {
-    if (!(outcode0 | outcode1)) {
+  do
+  {
+    if (!(outcode0 | outcode1))
+    {
       accept = true;
       done = true;
     } else if (outcode0 & outcode1)
       done = true;
-    else {
+    else
+    {
       double x, y;
       double m = (y1 - y0) / (x1 - x0);
       outcodeOut = outcode0 ? outcode0 : outcode1;
-      if (outcodeOut & TOP) {
+      if (outcodeOut & TOP)
+      {
         x = x0 + (1 / m) * (ymax - y0);
         y = ymax;
-      } else if (outcodeOut & BOTTOM) {
+      }
+      else if (outcodeOut & BOTTOM)
+      {
         x = x0 + (1 / m) * (ymin - y0);
         y = ymin;
-      } else if (outcodeOut & RIGHT) {
+      }
+      else if (outcodeOut & RIGHT)
+      {
         y = y0 + m * (xmax - x0);
         x = xmax;
-      } else {
+      }
+      else
+      {
         y = y0 + m * (xmin - x0);
         x = xmin;
       }
 
-      if (outcodeOut == outcode0) {
+      if (outcodeOut == outcode0)
+      {
         x0 = x;
         y0 = y;
         outcode0 = ComputeOutCode(x0, y0);
-      } else {
+      }
+      else
+      {
         x1 = x;
         y1 = y;
         outcode1 = ComputeOutCode(x1, y1);
@@ -80,7 +95,8 @@ void CohenSutherland(double x0, double y0, double x1, double y1) {
     glEnd();
   }
 }
-void display() {
+void display()
+{
   double x0 = 30, y0 = 20, x1 = 80, y1 = 120;
   double x3 = 80, y3 = 15, x4 = 45, y4 = 150;
   glClear(GL_COLOR_BUFFER_BIT);
@@ -103,12 +119,14 @@ void display() {
   CohenSutherland(x3, y3, x4, y4);
   glFlush();
 }
-void init() {
+void init()
+{
   glClearColor(1, 0.5, 0.5, 0.5);
   gluOrtho2D(0, 500, 0, 500);
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv)
+{
   glutInit( & argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(500, 500);
